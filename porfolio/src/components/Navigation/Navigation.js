c992027473051c6e51f297a7a799941a50d10c56
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-07 00:00:37 -0400

    modularized my src and installed my packages. Created Navigation and Header components.

---
+++ porfolio/src/components/Navigation/Navigation.js
@@ -0,0 +1,26 @@
+import "./Navigation.css";
+
+export default function Navigation() {
+  return (
+    <nav className="col container">
+      <ul className="row">
+        <li className="col list">
+          {" "}
+          <a href="google.com">About Me</a>
+        </li>
+        <li className="col list">
+          {" "}
+          <a href="google.com">Portfolio</a>
+        </li>
+        <li className="col list">
+          {" "}
+          <a href="google.com">Contact Me</a>
+        </li>
+        <li className="col list">
+          {" "}
+          <a href="google.com">Resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
