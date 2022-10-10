7033cbc35fb74ad5157a3846c9d9aa22a390bbe1
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-10 03:35:04 -0400

    added technologies/skills used and media queries to styles

--- porfolio/src/App.js
+++ porfolio/src/App.js
@@ -4,15 +4,15 @@ import "./components/Project/Project.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 // import Project from "./components/Project/Project";
-// import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
+// import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <div>
       <Header />
-      {/* <About /> */}
+      <About />
       {/* <Project /> */}
-      <Contact />
+      {/* <Contact /> */}
       <Footer />
     </div>
   );
