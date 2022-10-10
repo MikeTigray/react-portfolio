f74123496e47e8cb28774936b215b6d1bdf63ce0
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-10 01:20:57 -0400

    added email feature by installing emailjs

--- porfolio/src/components/Contact/Contact.js
+++ porfolio/src/components/Contact/Contact.js
@@ -1,18 +1,44 @@
 // import React, { useState } from "react";
+
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 import "./Contact.css";
 import { FiSend } from "react-icons/fi";
 export default function Contact() {
+  // Handling Email
+  const form = useRef();
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_upgpwli",
+        "template_dwu64xp",
+        form.current,
+        "qdzrHVHxkv7KBig_d8QJo"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
+
   return (
-    <div className="contact">
+    <form ref={form} className="contact">
       <div className="form">
         <p>Full name</p>{" "}
-        <input name="name" placeholder="Your name" type="text" />
+        <input name="user_name" placeholder="Your name" type="text" />
       </div>
       <div className="form">
         {" "}
         <p>Email address</p>
         <input
-          name="email"
+          name="user_email"
           placeholder="email-address@email.com"
           type="email"
         />
@@ -21,7 +47,15 @@ export default function Contact() {
         <p>Message</p>
         <textarea name="message" cols={25} rows={2}></textarea>
       </div>
-      <button className="submit">{<FiSend />}</button>
-    </div>
+
+      <button
+        type="submit"
+        value="Send"
+        onSubmit={sendEmail}
+        className="submit"
+      >
+        {<FiSend />}
+      </button>
+    </form>
   );
 }
