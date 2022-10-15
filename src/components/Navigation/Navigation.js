8abd9566acff81a8c357330645bfba85e9ce6caa
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-15 02:53:56 -0400

    made navbars to change color based on current location

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,30 +1,56 @@
 import "./Navigation.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export default function Navigation() {
+  let location = useLocation().pathname;
+  const [main, setMain] = useState(false);
+
+  const handleClick = () => {};
   return (
     <nav className="col container">
       <ul className="row">
         <li className="col list ">
           {" "}
-          <Link to="/Home" className="anchor">
+          <Link
+            id="home"
+            onClick={handleClick}
+            to="/Home"
+            className={location === "/Home" ? "target" : "anchor"}
+          >
             About Me()
           </Link>
         </li>
         <li className="col list">
           {" "}
-          <Link className="anchor" to="/project">
+          <Link
+            id="project"
+            onClick={handleClick}
+            className={location === "/project" ? "target" : "anchor"}
+            to="/project"
+          >
             Portfolio()
           </Link>
         </li>
         <li className="col list">
           {" "}
-          <Link className="anchor" to="/contact">
+          <Link
+            id="contact"
+            onClick={handleClick}
+            className={location === "/contact" ? "target" : "anchor"}
+            to="/contact"
+          >
             Contact Me()
           </Link>
         </li>
         <li className="col list">
           {" "}
-          <Link className="anchor" to="google.com">
+          <Link
+            id="resume"
+            onClick={handleClick}
+            className={location === "/resume" ? "target" : "anchor"}
+            to="/resume"
+          >
             Resume()
           </Link>
         </li>
